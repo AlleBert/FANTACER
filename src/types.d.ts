106be588ed93185799af547c5344a1a8678b9dfc
Alle <alessiobertolani.ab@gmail.com>
2026-05-28 14:03:36 +0200

    chore: remove local lucide-react type declarations (use package types instead)

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,30 +1,3 @@
-declare module 'lucide-react' {
-  export const Search: React.ComponentType<any>;
-  export const Loader2: React.ComponentType<any>;
-  export const ArrowUpDown: React.ComponentType<any>;
-  export const Download: React.ComponentType<any>;
-  export const Star: React.ComponentType<any>;
-  export const ChevronLeft: React.ComponentType<any>;
-  export const ChevronRight: React.ComponentType<any>;
-  export const Moon: React.ComponentType<any>;
-  export const Sun: React.ComponentType<any>;
-  export const Cookie: React.ComponentType<any>;
-  export const Trophy: React.ComponentType<any>;
-  export const Clock: React.ComponentType<any>;
-  export const Menu: React.ComponentType<any>;
-  export const X: React.ComponentType<any>;
-  export const LogOut: React.ComponentType<any>;
-  export const LayoutDashboard: React.ComponentType<any>;
-  export const Upload: React.ComponentType<any>;
-  export const Users: React.ComponentType<any>;
-  export const TrendingUp: React.ComponentType<any>;
-  export const TrendingDown: React.ComponentType<any>;
-  export const Minus: React.ComponentType<any>;
-  export const BarChart3: React.ComponentType<any>;
-  export const Palette: React.ComponentType<any>;
-  export const Vote: React.ComponentType<any>;
-}
-
 declare module 'recharts' {
   export const LineChart: any;
   export const Line: any;
