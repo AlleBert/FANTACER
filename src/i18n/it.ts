0140ef027b77da88be9780f46b1e464e5ede3d9a
Alle <alessiobertolani.ab@gmail.com>
2026-09-23 12:38:01 +0200

    copy(antibot): voto sospeso temporaneamente

--- src/i18n/it.ts
+++ src/i18n/it.ts
@@ -69,7 +69,7 @@ export const it: Dictionary = {
 
   'antibot.title': 'Voto sospeso',
   'antibot.body':
-    'I nostri sistemi hanno rilevato voti provenienti da sistemi automatizzati. Il voto è sospeso fino a domani mattina. Fantacer è un gioco: i sistemi automatici vanno contro le sue regole e il suo spirito. Le aziende che li utilizzano saranno escluse dalla classifica.',
+    'I nostri sistemi hanno rilevato voti provenienti da sistemi automatizzati. Il voto è sospeso temporaneamente. Fantacer è un gioco: i sistemi automatici vanno contro le sue regole e il suo spirito. Le aziende che li utilizzano saranno escluse dalla classifica.',
 
   'contact.title1': 'PARLA',
   'contact.title2': 'CON NOI',
